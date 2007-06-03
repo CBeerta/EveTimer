845035ddadb5957288003c026087528a6be2c08a
Claus Beerta <claus@beerta.de>
2007-06-03 06:34:47 +0000

    make EveXML work without data
    git-svn-id: svn://celaneo.local/EveTimer/trunk@169 0a101332-4a5c-4009-ba00-5ad6272eac1f

--- EveXML.py
+++ EveXML.py
@@ -25,32 +25,28 @@ class EveXML:
     skillnames = {}
     
     def __init__(self):
-        self.skilldom = minidom.parse('eve-skills2.xml')
+        try:
+            self.skilldom = minidom.parse('eve-skills2.xml')
+        except IOError:
+            self.skilldom = None
+        else:
+            node = self.skilldom.documentElement
+            for skill in node.getElementsByTagName('s'):
+                i = skill.getAttribute('i')
+                n = skill.getAttribute('n')
 
-        node = self.skilldom.documentElement
-        for skill in node.getElementsByTagName('s'):
-            i = skill.getAttribute('i')
-            n = skill.getAttribute('n')
-
-            if i.isdigit() and n != None:
-                self.skillnames[int(i)] = n
+                if i.isdigit() and n != None:
+                    self.skillnames[int(i)] = n
 
         
     def skillIdToName(self, id):
+
+        if self.skilldom == None:
+            return 'No skillinformation available'
+
         if id in self.skillnames.keys():
             return self.skillnames[id]
         elif id == None:
             return 'Not Training!'
         else:
             return 'Unknown Skill'
-
-
-
-
-if __name__ == '__main__':
-
-    xml = EveSkillXML()
-
-    print xml.trainingTypeID_to_string(12099)
-
-
